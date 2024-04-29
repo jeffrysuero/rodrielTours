42ae1f35e117aed30ba504a76f2a349b56295f5f
jeffrysuero <jeffrysueroc@gmail.com>
2024-04-29 10:11:39 -0400

    .

--- database/migrations/2024_02_29_140612_client.php
+++ database/migrations/2024_02_29_140612_client.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('lastName');
             $table->string('email');
             $table->string('phone');
-            $table->string('address');
+            // $table->string('address');
             $table->timestamps();
         });
     }
