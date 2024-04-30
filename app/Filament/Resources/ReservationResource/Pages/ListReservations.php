958a1e844591833bb6f795759907518ed3f0034c
jeffry suero <jeffrysueroc@gmail.com>
2024-04-30 16:40:49 -0400

    .

--- app/Filament/Resources/ReservationResource/Pages/ListReservations.php
+++ app/Filament/Resources/ReservationResource/Pages/ListReservations.php
@@ -64,6 +64,45 @@ class ListReservations extends ListRecords
             ];
         }
 
+        if ($user->roles[0]->name === 'Operador') {
+            return [
+                'SIN ASIGNAR' => Tab::make('Servicios sin Asignar')
+                    ->icon('heroicon-m-x-circle')
+                    ->badge(Reservation::where('status', 'SIN ASIGNAR')
+
+                        ->count() ?? 0)
+                    ->modifyQueryUsing(function (Builder $query) {
+                        return $query->where('status', 'SIN ASIGNAR');
+                    }),
+                'ASIGNADO' => Tab::make('Servicios Asignado a Choferes')
+                    ->icon('heroicon-m-user-circle')
+                    ->badge(Reservation::where('status', 'ASIGNADO')
+
+                        ->count() ?? 0)
+                    ->modifyQueryUsing(function (Builder $query) {
+                        return $query->where('status', 'ASIGNADO');
+                    }),
+
+                'EN PROGRESO' => Tab::make('Servicios en Progreso')
+                    ->icon('heroicon-m-exclamation-triangle')
+                    ->badge(Reservation::where('status', 'EN PROGRESO')
+
+                        ->count() ?? 0)
+                    ->modifyQueryUsing(function (Builder $query) {
+                        return $query->where('status', 'EN PROGRESO');
+                    }),
+
+                'COMPLETADO' => Tab::make('Servicios Completados')
+                    ->icon('heroicon-m-hand-thumb-up')
+                    ->badge(Reservation::where('status', 'COMPLETADO')
+
+                        ->count() ?? 0)
+                    ->modifyQueryUsing(function (Builder $query) {
+                        return $query->where('status', 'COMPLETADO');
+                    }),
+            ];
+        }
+
         if ($user->roles[0]->name === 'Conductores') {
             $user = Auth()->user();
             $vehicle = Vehicle::all()->where('userId', $user->id)->first();
