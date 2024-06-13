e02e7b7b8014ed5aa5c94dcc6e45cd7f02bab039
jeffry suero <jeffrysueroc@gmail.com>
2024-06-13 13:03:48 -0400

    up

--- app/Filament/Resources/ReservationResource.php
+++ app/Filament/Resources/ReservationResource.php
@@ -200,10 +200,11 @@ class ReservationResource extends Resource
                         ->icon('heroicon-m-document-minus')
                         ->iconColor('success')->alignCenter()
                         ->searchable(),
+                        Tables\Columns\TextColumn::make('airport')->icon('heroicon-m-building-office')->iconColor('primary'),
                     Tables\Columns\TextColumn::make('hotel')
                         ->icon('heroicon-m-paper-airplane')
                         ->iconColor('primary'),
-                    Tables\Columns\TextColumn::make('airport')->icon('heroicon-m-building-office')->iconColor('primary'),
+                    
                     Tables\Columns\TextColumn::make('arrivalDate')
                         ->icon('heroicon-m-clock')
                         ->iconColor('primary'),
